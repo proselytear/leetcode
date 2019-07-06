6e3da9bf56ef925bfaa99647a294f5c20e595724
Eugene Suleimanov <yevhen.suleimanov@chromeriver.com>
2019-07-06 20:27:20 +0300

    Added improved solution for problem #58

--- src/main/java/net/proselyte/leetcode/algorithms/strings/LengthOfLastWord.java
+++ src/main/java/net/proselyte/leetcode/algorithms/strings/LengthOfLastWord.java
@@ -2,7 +2,7 @@ package net.proselyte.leetcode.algorithms.strings;
 
 /**
  * Problem #58
- *
+ * <p>
  * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
  * <p>
  * If the last word does not exist, return 0.
@@ -17,7 +17,7 @@ package net.proselyte.leetcode.algorithms.strings;
 
 public class LengthOfLastWord {
     public static void main(String[] args) {
-        String input = "       ";
+        String input = "a";
         int result = lengthOfLastWord(input);
 
         System.out.println("Input: " + input);
@@ -25,16 +25,22 @@ public class LengthOfLastWord {
     }
 
     public static int lengthOfLastWord(String s) {
-        if(s == null){
-            throw new IllegalArgumentException("Invalid input");
-        }
-
-        String [] sMas = s.split(" ");
-
-        if(sMas.length == 0){
+        if (s == null || s.isEmpty()) {
             return 0;
         }
 
-        return sMas[sMas.length - 1].length();
+        int counter = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (counter > 0 && s.charAt(i) == ' ') {
+                break;
+            }
+
+            if (s.charAt(i) != ' ') {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 }
