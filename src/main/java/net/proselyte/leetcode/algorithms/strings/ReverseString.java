04dcbf0647283a717dc36be8880e2ef618209152
Eugene Suleimanov <yevhen.suleimanov@chromeriver.com>
2019-07-06 20:37:34 +0300

    Added problem #344 solution

---
+++ src/main/java/net/proselyte/leetcode/algorithms/strings/ReverseString.java
@@ -0,0 +1,42 @@
+package net.proselyte.leetcode.algorithms.strings;
+
+import java.util.Arrays;
+
+/**
+ * Problem #344
+ * <p>
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * You may assume all the characters consist of printable ascii characters.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ * Example 2:
+ * <p>
+ * Input: ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ */
+
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] input = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        System.out.println("Input: " + Arrays.toString(input));
+        reverseString(input);
+        System.out.println("Reversed: " + Arrays.toString(input));
+    }
+
+    public static void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+    }
+}
